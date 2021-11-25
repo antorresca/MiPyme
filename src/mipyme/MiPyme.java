f989ac711d286c00a6970ae4de19b140705a994f
Andres <Antorresca@unal.edu.co>
2021-11-25 16:30:00 -0500

    Principal

---
+++ src/mipyme/MiPyme.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mipyme;
+import static java.awt.Frame.MAXIMIZED_BOTH;
+import javax.swing.UnsupportedLookAndFeelException;
+import vistas.Sistema;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @authors Sergio, Andres, Julian, Marco
+ */
+public class MiPyme {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");//ESTA LINEA DA EL ESTILO PARA LOS FORMULARIOS.
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e){
+            
+           
+        }
+        Sistema sistema=new Sistema();  //OBJETO DEL FORMULARIO 
+        sistema.setExtendedState(MAXIMIZED_BOTH);//PARA ABRIR SIEMPRE EN VENTANA MAXIMIZADA
+        sistema.setVisible(true);
+       
+        
+    }
+    
+}
