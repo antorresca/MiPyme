ae730badf3852e06f4bd86d0278134177e6d00a7
DESKTOP-LL30ESM\Andres <antorresca@unal.edu.co>
2022-01-29 23:05:44 -0500

    G

--- src/interfaz/PantallaUsuarios.java
+++ src/interfaz/PantallaUsuarios.java
@@ -25,18 +25,26 @@ public class PantallaUsuarios {
 		JList listaUsuarios = new JList();
 		listaUsuarios.setModel(modelo4);
 		JScrollPane scroll = new JScrollPane(listaUsuarios);
-		scroll .setBounds(31, 41, 117, 200);
+		scroll .setBounds(70, 41, 117, 200);
 		pantallaUsuarios.getContenedor().add(scroll);
 		
 		pantallaUsuarios.update(pantallaUsuarios.getGraphics());
 		
-		Boton agregar = new Boton("Agregar",pantallaUsuarios,190, 105, 89, 23);
+		Boton agregar = new Boton("Agregar",pantallaUsuarios,210, 105, 89, 23);
+		agregar.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			pantallaUsuarios.setVisible(false);
+			PantallaRegistro.main(null);
+		}
+		});
 
-		Boton detalles = new Boton("Modificar",pantallaUsuarios,190, 139, 89, 23);
+		Boton detalles = new Boton("Modificar",pantallaUsuarios,210, 139, 89, 23);
+		
 
-		Boton pago = new Boton("Eliminar",pantallaUsuarios,190, 173, 89, 23);
+		Boton pago = new Boton("Eliminar",pantallaUsuarios,210, 173, 89, 23);
 
-		Boton regresarPedidos = new Boton("Regresar",pantallaUsuarios,190, 207, 89, 23);
+		Boton regresarPedidos = new Boton("Regresar",pantallaUsuarios,210, 207, 89, 23);
 		
 		regresarPedidos.addMouseListener(new MouseAdapter() { //regresar a pantalla anterior
 			@Override
