a353a33011c827ebefa11969dd0d5c1f9c02a8d4
maran <maran@DESKTOP-ASFRS5N>
2022-01-29 22:59:16 -0500

    Merge branch 'master' of https://github.com/antorresca/MiPyme

--- src/logica/Ejecucion.java
+++ src/logica/Ejecucion.java
@@ -35,7 +35,7 @@ public class Ejecucion {
 
 	private static String registroUsuariotxt;
 	//public static Usuario usuario_admin = new Usuario("Admin","12345");
-	public static Usuario usuario_admin = new Usuario("A","1",true);
+	public static Usuario usuario_admin = new Usuario("A","1");
 	public static Color ModoClaro = new Color(225,237,249); //Variable de colores (claro y oscuro)
 	public static Color ModoOscuro = new Color(20,31,59);
 	public static Color Tema = ModoClaro;
@@ -47,7 +47,9 @@ public class Ejecucion {
 	
 	@SuppressWarnings({ "unused", "rawtypes", "unchecked" })
 	public static void main(String[] args) {
+		usuario_admin.setAdmin(true);
 		usuarios.agregarA(usuario_admin);
+		
 				
 		
 		//--------------------------------------------PLANTILLAS-----------------------------------------------------------
